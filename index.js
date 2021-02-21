58aafa6193a6e076f3b35457ed836d8cc7ddaac0
syd333 <sydmiranda@icloud.com>
2021-02-21 14:42:42 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thankYou = []
+    for (let i = 0; i < names.length; i++) {
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYou
+}
+
+function countDown(num) {
+    while (num >= 0){
+        console.log(num)
+        num--
+    }
+
+}
